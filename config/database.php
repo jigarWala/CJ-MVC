098bd51745355a4a1e2acde78668c975e958d2f3
jigarWala <jigar.wala@somaiya.edu>
2017-10-20 23:23:50 +0530

    core CJ_Model class created with CRUD fuctions

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ $db_params = array(
 	'servername' => 'localhost',
 	'username' => 'root',
 	'password' => 'root@lamp',
-	'dbname' => 'forum'
+	'dbname' => 'test_forum'
 
 );
  ?>
---
+++ model/UserModel.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once(__DIR__.'/../test_core/CJ_Model.php');
+class UserModel extends CJ_Model{
+}
+ ?>
